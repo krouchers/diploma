4561b72ff091b5d8d45a53e916f1ba97c0f8b1e3
Krouchers <filin.rusya@mail.ru>
2023-02-06 17:18:58 +0500

    add glm dependency to cmake

---
+++ cmake/deps.cmake
@@ -0,0 +1,19 @@
+efec5db081e3aad807d0731e172ac597f6a39447
+
+include(FetchContent)
+
+FetchContent_Declare(
+    SDL3
+    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+    GIT_TAG release-2.26.2
+)
+
+FetchContent_Declare(
+    GLM 
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_TAG efec5db081e3aad807d0731e172ac597f6a39447
+)
+
+FetchContent_MakeAvailable(
+    SDL3 GLM
+)
