032843e8dd95f89ce5466f7f22c25ada91b23436
krouchers <filin.rusya@mail.ru>
2023-05-04 21:37:21 +0500

    added color header in common folder

---
+++ src/common/colors.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "glm/vec3.hpp"
+
+namespace Color
+{
+    inline glm::vec3 red{glm::vec3{163.f, 66, 81} / 255.f};
+    inline glm::vec3 blue{glm::vec3{64.f, 127.f, 193.f} / 255.f};
+    inline glm::vec3 baseplane{glm::vec3{71, 71, 71} / 255.f};
+    inline glm::vec3 white{glm::vec3{1, 1, 1}};
+    inline glm::vec3 outline{glm::vec3{1.0f, 0.0f, 0.0f}};
+}
