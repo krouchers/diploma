86e38b318c558421e9632493cd19ca2683d972ad
krouchers <filin.rusya@mail.ru>
2023-05-15 02:50:52 +0500

    implement instance class

--- src/opengl/include/opengl/instance.hpp
+++ src/opengl/include/opengl/instance.hpp
@@ -3,6 +3,7 @@
 
 #include "geometry/mesh.hpp"
 
+#include "glad.hpp"
 #include <vector>
 
 namespace gl
@@ -10,17 +11,22 @@ namespace gl
     class Instance
     {
     public:
-        Instance(utils::Data &&data);
-        void Add(glm::mat4x4 &transform, unsigned int id);
+        Instance(geometry::Mesh &&mesh);
+        void Add(glm::mat4x4 const &transform, unsigned int id);
+        void Render();
 
     private:
+        void Update();
         struct Info
         {
             glm::mat4x4 transform_;
-            unsigned int id;
+            unsigned int id_;
         };
 
         geometry::Mesh mesh_;
         std::vector<Info> data_;
+        bool dirty{true};
+
+        GLuint vbo_;
     };
 }
--- src/opengl/src/instance.cc
+++ src/opengl/src/instance.cc
@@ -2,13 +2,57 @@
 
 namespace gl
 {
-    void Instance::Add(glm::mat4x4 &transform, unsigned int id)
+    void Instance::Add(glm::mat4x4 const &transform, unsigned int id)
     {
         data_.push_back({transform, id});
     }
 
-    Instance::Instance(utils::Data &&data)
-        : mesh_{std::move(data)}
+    Instance::Instance(geometry::Mesh &&mesh)
+        : mesh_{std::move(mesh)}
     {
+        glGenBuffers(1, &vbo_);
+        glBindVertexArray(mesh_.GetVAO());
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+
+        glEnableVertexAttribArray(3);
+        glVertexAttribIPointer(3, 1, GL_UNSIGNED_INT,
+                               sizeof(Info),
+                               reinterpret_cast<void *>(offsetof(Info, id_)));
+        glVertexAttribDivisor(3, 1);
+
+        int base_ind = 4;
+        for (int i = 0; i < 4; ++i)
+        {
+            glEnableVertexAttribArray(base_ind + i);
+            glVertexAttribPointer(base_ind + i, 4, GL_FLOAT,
+                                  GL_FALSE, sizeof(Info),
+                                  reinterpret_cast<void *>(offsetof(Info, transform_) + i * sizeof(glm::vec4)));
+            glVertexAttribDivisor(base_ind + i, 1);
+        }
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+
+    void Instance::Update()
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_);
+        glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(Info) * data_.size(),
+                     data_.data(), GL_DYNAMIC_DRAW);
+
+        dirty = false;
+    }
+
+    void Instance::Render()
+    {
+        if (mesh_.dirty)
+            mesh_.Update();
+        if (dirty)
+            Update();
+
+        glBindVertexArray(mesh_.GetVAO());
+        glDrawElementsInstanced(GL_TRIANGLES, mesh_.IndicesSize(), GL_UNSIGNED_INT,
+                                nullptr, data_.size());
+        glBindVertexArray(0);
     }
 }
